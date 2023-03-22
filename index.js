00d598fa36759a41839a8589eea87b7045a5b920
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-03-22 23:28:52 +0530

    view and other controllers

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port=8000;
 
 //Use express router
-
 app.use('/', require('./routes/index'));
+
+//setup the view Engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
 
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const homeController = require('../controllers/home_controller');
 console.log("Index from Routes");
 
 router.get('/', homeController.home);
+//To maintain all the list of users in the file 
+router.use('/users', require('./users'));
 
 module.exports = router;
