c45f0e1b030bdf3945520b81d272deba7a4ad308
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-04-12 20:21:16 +0530

    Post Backend

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home_controller");
 
-console.log("Index from Routes");
-
+// console.log("Index from Routes");
 router.get("/", homeController.home);
-
 //To maintain all the list of users in the file
 router.use("/users", require("./users"));
+//To maintain the post
+router.use("/posts", require('./posts'));
 
 module.exports = router;
