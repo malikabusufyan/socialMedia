4bc3e2cd5069d4d282bf7a6d621940caa9ca4e00
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-04-13 14:56:55 +0530

    Partials and Deletions

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,47 @@ module.exports.create = async function(req, res) {
     return res.status(500).send('Internal Server Error');
   }
 };
+
+
+// module.exports.destroy = function(req, res){
+//   Comment.findById(req.params.id, function( err, comment){
+//     if(comment.user == req.user.id){
+//       let postId = comment.post;
+//       comment.deleteOne();
+
+//       Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, function(err, post){
+//         return res.redirect('back');
+//       })
+//     }else{
+//       return res.redirect('back');
+//     }
+//   })
+// }
+
+module.exports.destroy = async function (req, res) {
+  try {
+    let comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+      return res.redirect('back');
+    }
+
+    if (comment.user != req.user.id) {
+      return res.redirect('back');
+    }
+
+    let postId = comment.post;
+    await comment.deleteOne();
+
+    let post = await Post.findByIdAndUpdate(
+      postId,
+      { $pull: { comments: req.params.id } },
+      { new: true }
+    );
+
+    return res.redirect('back');
+  } catch (err) {
+    console.error('Error in deleting comment:', err);
+    return res.redirect('back');
+  }
+};
