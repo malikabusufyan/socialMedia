d7493185593974e705a9080069ec8d3b49334baf
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-04-27 13:36:46 +0530

    Creating Web Token

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 //Accessing MongoStore from the MongoConnect Library to store session
 const MongoStore = require('connect-mongo');
 /*
